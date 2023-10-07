7db7a2aeb9412ba76f95cde6a80b325cd9a4eac8
Nicolas Coffez <ncoffez@gmail.com>
2023-10-07 20:30:40 +0200

    Some minor changes to nuxt composables

--- app.vue
+++ app.vue
@@ -2,28 +2,18 @@
 <main class="container">
 	<div class="board" id="problem" v-if="!showSolution">
 		<h2>Sudoku of the day</h2>
-		<div class="row" v-for="row of sudoku.problem.value">
-			<div class="field" v-for="(field, index) of row">
-				<span v-if="field !== null">{{ field }}</span>
-				<input type="text" v-else />
-			</div>
-		</div>
+		<Grid :grid="sudoku.problem.value" />
 	</div>
 	<div class="board" id="solution" v-if="showSolution">
 		<h2>Solution</h2>
-		<div class="row" v-for="row of sudoku.solution.value">
-			<div class="field" v-for="field of row">
-				{{ field === null ? '' : field }}
-			</div>
-		</div>
+		<Grid :grid="sudoku.solution.value" />
 	</div>
-	<a id="show-solution" class="secondary outline" href="#" role="button" @click="showSolution = !showSolution">Show
-		Solution</a>
+	<button id="show-solution" class="secondary outline" @click="showSolution = !showSolution">Show Solution</button>
 	<button id="new-grid" @click="newGrid()">Create new grid</button>
 </main>
 </template>
 <script lang="ts" setup>
-import { Sudoku } from '~/utils/sudoku'
+import { Sudoku } from '~/composables/sudoku'
 
 const showSolution = ref(false)
 let sudoku: Ref<Sudoku> = ref(new Sudoku())
---
+++ components/Grid.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="row" v-for="row of props.grid">
+  <div class="field" v-for="(field, index) of row">
+    <span v-if="field !== null">{{ field }}</span>
+    <input type="text" v-else />
+  </div>
+</div>
+</template>
+<script lang='ts' setup>
+const props = defineProps(['grid'])
+
+</script>
+<style lang='sass' scoped></style>
